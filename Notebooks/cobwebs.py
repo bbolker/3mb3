55ce8359e59e8d33b5759e2aa745e5beb35a2b76
Ben Bolker <bbolker@gmail.com>
2017-09-18 11:38:48 -0400

    cobwebbing update

--- Notebooks/cobwebs.py
+++ Notebooks/cobwebs.py
@@ -4,10 +4,25 @@ import matplotlib.pyplot as plt
 from matplotlib.collections import LineCollection
 
 def cobweb(fun, x0=1, parms=None, nt=20, maxval=None):
+    '''basic cobwebbing program: single starting value, single parameter set
+    fun: function fun(x,parms) taking a state variable (x) and a tuple
+         of parameters (parms)
+    x0: starting value
+    parms: tuple of parameter values
+    nt: number of time steps
+    maxval: maximum value for x/y axes
+    '''
     fancy_cobweb(fun, x0 = (x0,), parms = (parms,), nt=nt, maxval=maxval)
     return(None)
 
 def fancy_cobweb(fun, x0=(1,), parms=(None,),nt=20, maxval=None):
+    '''fancy cobwebbing program: takes multiple starting values (as
+    a tuple and/or parameter values (as a tuple of tuples). The 
+    shorter of these tuples will be replicated to the length of the longer.
+    fun, nt, maxval: as in basic cobweb
+    x0: a tuple (possibly length 1) of starting values
+    parms: a tuple (possibly length 1) of tuples of parameters
+    '''
     ## assemble a list of lists of tuples
     lines = []
     traj = []
